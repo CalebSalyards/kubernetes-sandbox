09345f13a9b9190a1185b93c57c336aa44a71d10
salsonn <caleb.salyards@outlook.com>
2025-06-03 14:26:52 -0600

    Add kubectl install

--- kind-install.sh
+++ kind-install.sh
@@ -2,3 +2,7 @@
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-$(uname)-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
+
+# Installing Kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
